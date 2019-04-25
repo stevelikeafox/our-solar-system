ccf8a7bfd96620843534c33fd683fb142fea57dc
stevelikeafox <stephenwillmot@googlemail.com>
2019-04-25 02:36:15 +0100

    material design added, nav comp added and inital styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
 
 class App extends Component {
   state = {
@@ -19,14 +20,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <h1>CARDS</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col s6 offset-s3">
 
-        {this.state.cards.map(cards =>
-
-          <h1 key={cards._id}> {cards.cards.fact} </h1>)
-
-        }
-
+              {this.state.cards.map(cards => <div className="card" key={cards._id}>
+                <div className="card-image waves-effect waves-block waves-light">
+                  <img className="activator" src={cards.cards.img}></img>
+                </div>
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">{cards.cards.fact}<i className="material-icons right">more_vert</i></span>
+                  <p><a href="#">This is a link</a></p>
+                </div>
+                <div className="card-reveal">
+                  <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                  <p>{cards.cards.moreInfo}</p>
+                </div>
+              </div>)}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
