7858c59d6e9738d8151b1ebeb2742f9db0975e77
stevelikeafox <stephenwillmot@googlemail.com>
2019-05-14 23:01:43 +0100

    initial commit

--- server.js
+++ server.js
@@ -158,6 +158,19 @@ app.delete('/cards/:cardid', (req, res, next) => {
 });
 
 
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+
+
+
 app.listen(PORT, function () {
     console.log('Starting Server on: ' + PORT);
 });
